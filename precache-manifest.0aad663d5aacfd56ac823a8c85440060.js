3b3487e6f358ecad5a20ce7ffebb284129431b22
Enes Tüfekçi <enesxtufekci@gmail.com>
2020-07-05 20:19:34 +0200

    Updates

---
+++ precache-manifest.0aad663d5aacfd56ac823a8c85440060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3208cbe6222c93d5cbdd5d2d34e747",
+    "url": "/stunning-potato/index.html"
+  },
+  {
+    "revision": "8a22a43c0d8d13e62177",
+    "url": "/stunning-potato/static/css/main.ab7098ee.chunk.css"
+  },
+  {
+    "revision": "fcc2c5dd7dd3ea2bbb87",
+    "url": "/stunning-potato/static/js/2.5ff6a49e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stunning-potato/static/js/2.5ff6a49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a22a43c0d8d13e62177",
+    "url": "/stunning-potato/static/js/main.87d2b054.chunk.js"
+  },
+  {
+    "revision": "a653b20b1150d30d08e1",
+    "url": "/stunning-potato/static/js/runtime-main.931ba01f.js"
+  }
+]);
